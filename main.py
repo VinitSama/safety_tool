335b59cae9ca85f9f6991ec4dbac53cd58d43674
VinitSama <vinitaggarwal08072002@gmail.com>
2024-12-09 18:21:09 +0530

    update - alarm

--- main.py
+++ main.py
@@ -1,88 +1,51 @@
 import cv2
 import os
-
-# oneDNN custom operations are on.
-# You may see slightly different numerical results due to floating-point round-off errors from different computation orders.
-# To turn them off, set the environment variable `TF_ENABLE_ONEDNN_OPTS=0`
-os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
-
-import datetime as dt
+import winsound
 import numpy as np
 import tensorflow as tf
-
-
-
-
-
-
-
-
-
-import cv2
-import numpy as np
-
 from keras.models import load_model
-# from keras.preprocessing.image import img_to_array
 from tensorflow.keras.utils import img_to_array
-from playsound import playsound
-from threading import Thread
+import torch
+from keras.models import load_model
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
+print("Tensorflow version:", tf.__version__)
 
 
-def start_alarm(sound):
-    """Play the alarm sound"""
-    playsound('data/alarm.mp3')
-
-
-classes = ['Closed', 'Open']
 face_cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml")
 left_eye_cascade = cv2.CascadeClassifier("data/haarcascade_lefteye_2splits.xml")
 right_eye_cascade = cv2.CascadeClassifier("data/haarcascade_righteye_2splits.xml")
-# cap = cv2.VideoCapture(0)
-INPUT_VIDEO = 'sample/test_2.mp4'
-cap = cv2.VideoCapture(INPUT_VIDEO)
 model1 = load_model("drowiness_new7.h5")
+OBJECT_DETECTION_MODEL_PATH = "models/best.pt"
+PREDICTOR_MODEL_PATH = "models/keras_model.h5"
+model = torch.hub.load("ultralytics/yolov5", "custom", path=OBJECT_DETECTION_MODEL_PATH)
+print("Script loaded. Import complete")
+
+classes = ['Closed', 'Open']
 count = 0
 alarm_on = False
-alarm_sound = "data/alarm.mp3"
+alarm_sound = "data/alarm.wav"
 status1 = ''
 status2 = ''
 
 
 
+cap = cv2.VideoCapture(0)
+
+# INPUT_VIDEO = 'sample/test_1.mp4'
+# cap = cv2.VideoCapture(INPUT_VIDEO)
 
 
 
 
-
-
-
-
-
-
-
-
-print("Tensorflow version:", tf.__version__)
-
-import torch
-from keras.models import load_model
-
-print("Script loaded. Import complete")
-
-OBJECT_DETECTION_MODEL_PATH = "models/best.pt"
-PREDICTOR_MODEL_PATH = "models/keras_model.h5"
 CLASS_NAMES = {0: "No Seatbelt worn", 1: "Seatbelt Worn"}
 
 # Threshold score for the predictor model
 THRESHOLD_SCORE = 0.99
 
-SKIP_FRAMES = 1  # skips every 2 frames
 MAX_FRAME_RECORD = 500
-INPUT_VIDEO = 'sample/test_1.mp4'
-OUTPUT_FILE = (
-    "output/test_result_"
-    + dt.datetime.strftime(dt.datetime.now(), "%Y%m%d%H%M%S")
-    + ".mp4"
-)
+
 
 # Colors for drawing bounding boxes
 COLOR_GREEN = (0, 255, 0)
@@ -110,23 +73,12 @@ def prediction_func(img):
 predictor = load_model(PREDICTOR_MODEL_PATH, compile=False)
 print("Predictor loaded")
 
-# Ultralytics object detection model : https://docs.ultralytics.com/yolov5/
-model = torch.hub.load(
-    "ultralytics/yolov5", "custom", path=OBJECT_DETECTION_MODEL_PATH, force_reload=True
-)
-
-# Load the video capture
-# cap = cv2.VideoCapture(INPUT_VIDEO)
 # Get the frame width and height
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 # Get the size of the video
 size = (frame_width, frame_height)
 
-# Create the output directory if it doesn't exist
-# os.makedirs(OUTPUT_FILE.rsplit("/", 1)[0], exist_ok=True)
-# Create the video writer
-# writer = cv2.VideoWriter(OUTPUT_FILE, cv2.VideoWriter_fourcc(*'MP4V'), 30, size)
 
 print("Analyzing input video...")
 
@@ -185,9 +137,6 @@ while True:
     # Read a frame from the video
     ret, frame = cap.read()
 
-
-
-
     if not ret:
         break
 
@@ -209,8 +158,6 @@ while True:
             eye1 = np.expand_dims(eye1, axis=0)
             pred1 = model1.predict(eye1)
             status1 = np.argmax(pred1)
-            # print(status1)
-            # status1 = classes[pred1.argmax(axis=-1)[0]]
             break
 
         for (x2, y2, w2, h2) in right_eye:
@@ -222,8 +169,6 @@ while True:
             eye2 = np.expand_dims(eye2, axis=0)
             pred2 = model1.predict(eye2)
             status2 = np.argmax(pred2)
-            # print(status2)
-            # status2 = classes[pred2.argmax(axis=-1)[0]]
             break
 
         # If the eyes are closed, start counting
@@ -238,25 +183,18 @@ while True:
                             2)
                 if not alarm_on:
                     alarm_on = True
-                    # play the alarm sound in a new thread
-                    t = Thread(target=start_alarm, args=(alarm_sound,))
-                    t.daemon = True
-                    t.start()
+                    #play alarm
+                    winsound.PlaySound(alarm_sound, winsound.SND_ASYNC)
+
+
         else:
             cv2.putText(frame, "Eyes Open", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1)
             count = 0
+            #stop alarm if it is ON
             alarm_on = False
+            winsound.PlaySound(None, winsound.SND_PURGE)
 
-    # cv2.imshow("Drowsiness Detector", frame)
-
-
-
-
-
-
-
-    # If the frame is read successfully
-
+    
     # Increment the frame count
     frame_count += 1
 
@@ -273,10 +211,9 @@ while True:
         break
 
 
-cap.release()
+#cap.release()
 # writer.release()
 
 # Destroy all the windows
 cv2.destroyAllWindows()
 
-print("Script run complete. Results saved to :", OUTPUT_FILE)
